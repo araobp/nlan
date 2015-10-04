f91d26616bbc0505bc7718248eec32166b5f6b28
Shinya Arao <araobp@users.noreply.github.com>
2015-10-05 00:14:36 +0900

    HelloWorld server

--- protoc.sh
+++ protoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc -I ./helloworld/helloworld ./helloworld/helloworld/helloworld.proto --go_out=plugins=grpc:helloworld
+protoc -I ./helloworld/helloworld ./helloworld/helloworld/helloworld.proto --go_out=plugins=grpc:helloworld/helloworld

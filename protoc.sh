9f9d0dd9018d15f1cbef482af3ccef328ea329cb
Shinya Arao <araobp@users.noreply.github.com>
2015-10-05 18:31:34 +0900

    grpc-test

--- protoc.sh
+++ protoc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-protoc -I ./helloworld/helloworld ./helloworld/helloworld/helloworld.proto --go_out=plugins=grpc:helloworld/helloworld
+#protoc -I ./helloworld/helloworld ./helloworld/helloworld/helloworld.proto --go_out=plugins=grpc:helloworld/helloworld
+protoc -I ./grpc-test/api ./grpc-test/api/agent_api.proto --go_out=plugins=grpc:grpc-test/api

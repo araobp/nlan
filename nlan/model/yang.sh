0ff9b167dcfc52b06d0545adf6ed20a50020d326
Shinya Arao <araobp@users.noreply.github.com>
2015-10-11 17:16:08 +0900

    NLAN YANG model => protobuf3 with rpc

---
+++ nlan/model/yang.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "--- PYANG ---"
+pyang --format=tree */*.yang
+
+echo ""
+echo "--- GOYANG ---"
+goyang --format=tree */*.yang
+
+echo ""
+echo "--- GOYANG (proto) --"
+goyang --format=proto */*.yang
+
+# Add "syntex", since goyang does not add it
+# Add "rpc", since goyang does not seem to support YANG rpc
+cat nlan/rpc.proto > nlan/nlan.proto
+# Remove "option", since protobuf3 does not allow it
+goyang --format=proto nlan/nlan.yang | awk '{gsub("option ", "");print}' >> nlan/nlan.proto
+
+

1e4d86954a780551d7dcbec9c5c1373ee7ab4fe2
Shinya Arao <araobp@users.noreply.github.com>
2015-10-15 20:11:11 +0900

    Minimal RFC7047 implementation.

--- misc/ovsdb-test/ovsdb-test.go
+++ misc/ovsdb-test/ovsdb-test.go
@@ -1,32 +1,80 @@
+// 2015/10/15
+// Minimal RFC7047(OVSDB mgmt protocol) implementation for one-shot operation.
 package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
-	_ "net"
-
-	"github.com/socketplane/libovsdb"
+	"net"
 )
 
+const (
+	DATABASE = "Open_vSwitch"
+	SOCK     = "/var/run/openvswitch/db.sock"
+)
+
+type jsonrpc struct {
+	Method string        `json:"method"`
+	Params []interface{} `json:"params"`
+	Id     int           `json:"id"`
+}
+
+type Operation struct {
+	Op    string        `json:"op"`
+	Table string        `json:"table"`
+	Where []interface{} `json:"where"`
+}
+
+func Condition(column string, function string, value interface{}) []interface{} {
+	return []interface{}{column, function, value}
+}
+
+func read(conn net.Conn) []byte {
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	response := buf[0:n]
+	return response
+}
+
+var id int = 0
+
+// Synchronous JSON-RPC request to OVSDB.
+// Note: root privilege required for executing this API.
+func RequestSync(method string, params []interface{}) []byte {
+	conn, err := net.Dial("unix", SOCK)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	id++
+	rpc := jsonrpc{
+		Method: method,
+		Params: params,
+		Id:     id,
+	}
+	json_data, err := json.Marshal(rpc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Request: %s\n", string(json_data))
+	fmt.Fprintf(conn, string(json_data))
+	response := read(conn)
+	log.Printf("Response: %s\n", string(response))
+	return response
+}
+
 func main() {
-	//conn, err := net.Dial("tcp", "localhost:6379")
-	//if err != nil {
-	//}
-	type Rpc struct {
-		method string
-		params [][]byte
-		id     int
-	}
-	condition := libovsdb.NewCondition("vni", "==", 1001)
-	ope := libovsdb.Operation{
+	_ = RequestSync("list_dbs", []interface{}{})
+
+	cond := Condition("vni", "==", 1001)
+	ope := Operation{
 		Op:    "select",
-		Table: "interface",
-		Where: condition,
+		Table: "Interface",
+		Where: cond,
 	}
-	p := []libovsdb.Operation{ope}
-	p0, _ := json.Marshal(p)
-	log.Printf("%v\n", string(p0))
-	p := Rpc{method: "transact", params: [][]byte{p0}, id: 1}
-	json_data, _ := json.Marshal(p)
-	log.Printf("%v\n", string(json_data))
+	_ = RequestSync("transact", []interface{}{DATABASE, ope})
 }

7ce198862736a4d035867890febedfe6e0b8647f
Shinya Arao <araobp@users.noreply.github.com>
2015-10-15 22:14:24 +0900

    Goroutine

--- nlan/master/main.go
+++ nlan/master/main.go
@@ -5,7 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
-	_ "fmt"
+	"fmt"
 	"io/ioutil"
 	"log"
 	_ "os"
@@ -16,25 +16,12 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
-	target := flag.String("target", "localhost", "target host")
-	state := flag.String("state", "state.json", "state file")
-	service := flag.String("service", "ptn", "model")
-	flag.Parse()
-	log.Println(*target)
-	log.Println(*state)
-	log.Println(*service)
-
-	// Connects to the target host
-	var buffer bytes.Buffer
-	buffer.WriteString(*target)
-	buffer.WriteString(env.PORT)
-	address := buffer.String()
-	log.Printf("target: %s\n", address)
+func deploy(address string, ope int, state *string, c chan<- int) {
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Print(err)
 	}
+	defer conn.Close()
 	agent := nlan.NewNlanAgentClient(conn)
 
 	// Reads the state file and converts it into a Go struct
@@ -52,9 +39,41 @@ func main() {
 
 	// NLAN Request
 	request := nlan.Request{Model: &model}
-	response, err := agent.Add(context.Background(), &request)
+	var response *nlan.Response
+	switch ope {
+	case env.ADD:
+		response, err = agent.Add(context.Background(), &request)
+	case env.UPDATE:
+		response, err = agent.Update(context.Background(), &request)
+	case env.DELETE:
+		response, err = agent.Delete(context.Background(), &request)
+	}
 	if err != nil {
 		log.Print(err)
 	}
 	log.Print(response)
+	c <- 0
+}
+
+func main() {
+	target := flag.String("target", "localhost", "target host")
+	state := flag.String("state", "state.json", "state file")
+	service := flag.String("service", "ptn", "model")
+	flag.Parse()
+	log.Println(*target)
+	log.Println(*state)
+	log.Println(*service)
+
+	// Connects to the target host
+	var buffer bytes.Buffer
+	buffer.WriteString(*target)
+	buffer.WriteString(env.PORT)
+	address := buffer.String()
+	log.Printf("target: %s\n", address)
+
+	// Deployment
+	c := make(chan int)
+	go deploy(address, env.ADD, state, c)
+	value, _ := <-c
+	fmt.Printf("chan: %d\n", value)
 }
---
+++ nlan/master/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./main -state ../etc/ptn.json

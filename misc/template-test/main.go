53afc389f38f518078a2aa1b49529120c8b9419d
Shinya Arao <araobp@users.noreply.github.com>
2015-10-20 06:50:10 +0900

    template-test update

--- misc/template-test/main.go
+++ misc/template-test/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
-	"io/ioutil"
+	_ "io/ioutil"
+	"os"
 	"text/template"
 )
 
@@ -16,7 +18,18 @@ func main() {
 	v["router2"] = "192.168.56.102"
 	v["router3"] = "192.168.56.103"
 
-	roster_in, _ := ioutil.ReadFile(filename)
+	//roster_in, _ := ioutil.ReadFile(filename)
+	fp, _ := os.Open(filename)
+	defer fp.Close()
+	reader := bufio.NewReader(fp)
+	head, _, _ := reader.ReadLine()
+	fmt.Println("--- 1st line ---")
+	fmt.Println(string(head))
+	fmt.Println("----------------")
+	fp.Seek(0, 0)
+	reader = bufio.NewReader(fp)
+	roster_in := make([]byte, 4096)
+	_, _ = reader.Read(roster_in)
 	templ, _ := template.New("roster").Parse(string(roster_in))
 	var roster_out bytes.Buffer
 	_ = templ.Execute(&roster_out, v)

a1e60bcc09b316632aefe8d16196c20c630b25aa
Shinya Arao <araobp@users.noreply.github.com>
2015-11-08 23:20:21 +0900

    Zfill func

---
+++ nlan/util/zfill.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Python zfill-like function
+func Zfill(s string, pad string, overall int) string {
+	l := overall - len(s)
+	return strings.Repeat(pad, l) + s
+}
+
+// Converts IP address into zero-padded string.
+// For example, "10.1.2.3" => "010001002003"
+func ZfillIp(ip string) string {
+	splited := strings.Split(ip, ".")
+	for i, s := range splited {
+		splited[i] = Zfill(s, "0", 3)
+	}
+	return strings.Join(splited, "")
+}

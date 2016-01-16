70de55713a704383b57d802ef2e9f1d8b95c3330
Shinya Arao <araobp@users.noreply.github.com>
2016-01-17 02:17:26 +0900

    IP Address Management plugin

---
+++ plugins/ipam/plugins/ipam.py
@@ -0,0 +1,41 @@
+import tega.tree
+import tega.subscriber
+
+class IpAddressManagement(tega.subscriber.PlugIn):
+    '''
+    Manages IP addresses of Linux containers
+    '''
+
+    def __init__(self):
+        super().__init__()
+
+    def initialize(self):
+        nlan = tega.tree.Cont('nlan')
+        with self.tx() as t:
+            nlan.ipam = self.func(self._gen)  # Attached to nlan.ipam
+            t.put(nlan.ipam)
+
+    def on_notify(self, notifications):
+        pass
+
+    def on_message(self, channel, tega_id, message):
+        pass
+
+    MASK = '24'
+
+    def _gen(self, ip, *routers):
+        '''
+        IP address generation
+        '''
+        nlan = tega.tree.Cont('nlan')
+        with self.tx() as t:
+            abcd = ip.split('.')
+            d = abcd[3]
+            abc = abcd[:2]
+            for r in routers:
+                nextip = abcd[:3]
+                nextip.append(d)
+                nlan.ip[r] = '{}/{}'.format('.'.join(nextip), self.MASK)
+                t.put(nlan.ip[r])
+                d = str(int(d) + 1)
+

3babea304d0229f24b10a1579cdd9515d0ade581
Shinya Arao <araobp@users.noreply.github.com>
2016-01-24 08:43:59 +0900

    A plugin for Sigma Js: 2nd commit.

--- plugins/nlan/plugins/sigmajs.py
+++ plugins/nlan/plugins/sigmajs.py
@@ -17,7 +17,18 @@ class SigmaJs(tega.subscriber.PlugIn):
 
     def on_notify(self, notifications):
         # You write the data transformation script here.
-        print(notifications)
+        state = notifications[0]['instance']
+        for router, model in state.items():
+            print('')
+            print('router: {}'.format(router))
+            m = model['Ptn']['Networks']
+            for n in m:
+                id_ = n['Id']
+                links = n['Links']
+                local_ip = links['LocalIp']
+                remote_ips = links['RemoteIps']
+                print('local_ip: {}'.format(local_ip))
+                print('remote_ips: {}'.format(remote_ips))
 
     def on_message(self, channel, tega_id, message):
         pass

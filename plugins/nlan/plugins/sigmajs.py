660b4300aa506bb21e5d9ff8cc0a290683445444
Shinya Arao <araobp@users.noreply.github.com>
2016-01-24 14:30:03 +0900

    A plugin for Sigma Js: final commit.

--- plugins/nlan/plugins/sigmajs.py
+++ plugins/nlan/plugins/sigmajs.py
@@ -1,6 +1,9 @@
 import tega.tree
+import tega.util
 import tega.subscriber
 from tega.subscriber import SCOPE
+
+import json
 
 class SigmaJs(tega.subscriber.PlugIn):
     '''
@@ -16,19 +19,33 @@ class SigmaJs(tega.subscriber.PlugIn):
         self.subscribe('nlan.state', SCOPE.GLOBAL)
 
     def on_notify(self, notifications):
-        # You write the data transformation script here.
+        '''
+        nlan.state --> Sigma js vertexs/edges transformation
+        '''
         state = notifications[0]['instance']
+        nodes = [] 
+        edges = [] 
+        network = {'nodes': nodes, 'edges': edges}
+
         for router, model in state.items():
-            print('')
-            print('router: {}'.format(router))
             m = model['Ptn']['Networks']
+            seq = 0
+            local_ip=''
             for n in m:
                 id_ = n['Id']
                 links = n['Links']
                 local_ip = links['LocalIp']
                 remote_ips = links['RemoteIps']
-                print('local_ip: {}'.format(local_ip))
-                print('remote_ips: {}'.format(remote_ips))
+                for remote_ip in remote_ips:
+                    edges.append(dict(id='n'+str(seq), source=local_ip, target=remote_ip))
+                    seq += 1
+            nodes.append(dict(id=local_ip))
+
+        # Puts the transformed data on tega db
+        cont = tega.util.subtree('nlan.topo', network)
+        with self.tx() as t:
+            t.put(cont)
+            print(cont)
 
     def on_message(self, channel, tega_id, message):
         pass

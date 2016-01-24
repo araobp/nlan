6b072cd9b9664af414127bf5a3ac0cab4b7ab293
Shinya Arao <araobp@users.noreply.github.com>
2016-01-25 00:57:28 +0900

    Update, but dead-lock occurs when modifiying nlan.state...

--- plugins/nlan/plugins/sigmajs.py
+++ plugins/nlan/plugins/sigmajs.py
@@ -2,8 +2,10 @@ import tega.tree
 import tega.util
 import tega.subscriber
 from tega.subscriber import SCOPE
+from tega.idb import OPE
 
 import json
+import logging
 
 class SigmaJs(tega.subscriber.PlugIn):
     '''
@@ -22,30 +24,45 @@ class SigmaJs(tega.subscriber.PlugIn):
         '''
         nlan.state --> Sigma js vertexs/edges transformation
         '''
-        state = notifications[0]['instance']
-        nodes = [] 
-        edges = [] 
-        network = {'nodes': nodes, 'edges': edges}
+        print(notifications)
+        notification = notifications[0]
+        state = notification['instance']
+        ope = notification['ope']
+        path = notification['path']
 
-        for router, model in state.items():
-            m = model['Ptn']['Networks']
-            seq = 0
-            local_ip=''
-            for n in m:
-                id_ = n['Id']
-                links = n['Links']
-                local_ip = links['LocalIp']
-                remote_ips = links['RemoteIps']
-                for remote_ip in remote_ips:
-                    edges.append(dict(id='n'+str(seq), source=local_ip, target=remote_ip))
-                    seq += 1
-            nodes.append(dict(id=local_ip))
+        print(ope)
+        print(path)
+        if ope == OPE.DELETE.name and path == 'nlan.state': 
+            with self.tx() as t:
+                t.delete('nlan.topo')
+                logging.info('nlan.topo deleted')
+        else: 
+            if path != 'nlan.state':
+                state = idb.get(path='nlan.state')
+                logging.info('nlan.state got')
+            nodes = [] 
+            edges = [] 
+            network = {'nodes': nodes, 'edges': edges}
 
-        # Puts the transformed data on tega db
-        cont = tega.util.subtree('nlan.topo', network)
-        with self.tx() as t:
-            t.put(cont)
-            print(cont)
+            for router, model in state.items():
+                m = model['Ptn']['Networks']
+                seq = 0
+                local_ip=''
+                for n in m:
+                    id_ = n['Id']
+                    links = n['Links']
+                    local_ip = links['LocalIp']
+                    remote_ips = links['RemoteIps']
+                    for remote_ip in remote_ips:
+                        edges.append(dict(id='n'+str(seq), source=local_ip, target=remote_ip))
+                        seq += 1
+                nodes.append(dict(id=local_ip))
+
+            # Puts the transformed data on tega db
+            cont = tega.util.subtree('nlan.topo', network)
+            with self.tx() as t:
+                t.put(cont)
+                logging.info('nlan.topo put')
 
     def on_message(self, channel, tega_id, message):
         pass
